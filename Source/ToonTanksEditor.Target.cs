d55f45a46246b37eec6ba918567b37c59a274628
Mateus Sartori <98856859+Mathiuw@users.noreply.github.com>
2025-07-17 23:35:50 -0300

    1.0.0

--- Source/ToonTanksEditor.Target.cs
+++ Source/ToonTanksEditor.Target.cs
@@ -8,7 +8,7 @@ public class ToonTanksEditorTarget : TargetRules
 	public ToonTanksEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 
 		ExtraModuleNames.AddRange( new string[] { "ToonTanks" } );
